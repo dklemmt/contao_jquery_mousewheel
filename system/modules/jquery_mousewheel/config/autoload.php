df89da2dc519b48c4c59682dab0a7a908981b749
Dirk Klemmt <dirch@quantentunnel.de>
2012-10-11 17:33:01 +0200

    initial import

---
+++ system/modules/jquery_mousewheel/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_mousewheel 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_mousewheel' => 'system/modules/jquery_mousewheel/templates',
+));
---
+++ system/modules/jquery_mousewheel/languages/de/modules.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   jquery_mousewheel 
+ * @author    Dirk Klemmt 
+ * @license   GPL 
+ * @copyright Dirk Klemmt 2012 
+ */
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['jquery_mousewheel'] = array('jQuery Mousewheel Plugin', '');
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['jquery_mousewheel'] = array('jQuery Mousewheel Plugin', '');
